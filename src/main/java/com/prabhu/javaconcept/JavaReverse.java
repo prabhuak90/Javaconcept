8f88f8bf35c1fc66ba1e08a631cdef62455a4882
Prabhu <prabhuak90@gmail.com>
2022-08-20 17:54:48 +0530

    adding java reverse function

---
+++ src/main/java/com/prabhu/javaconcept/JavaReverse.java
@@ -0,0 +1,16 @@
+package com.prabhu.javaconcept;
+
+public class JavaReverse {
+
+    public static void main(String[] args) {
+
+        String name="prabhu";
+
+        for(int i=name.length()-1; i>=0;i--){
+            System.out.print(name.charAt(i));
+        }
+        
+
+    }
+
+}
